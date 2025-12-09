8c63e7a6447ac5bcdf8d7a1c1cbc4b838f94842e
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-12-09 12:48:13 +0500

    moveda all components into their own seperate files

---
+++ chatbot-project/src/components/ChatInput.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import { Chatbot } from 'supersimpledev'
+import LoadingGif from '../assets/loading-spinner.gif';
+
+export function ChatInput({chatMessages, setChatMessages}){
+  const [inputText, setInputText] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  function saveInputText(event) {
+    setInputText(event.target.value);
+  }
+
+  function handleKeyDown(event){
+    if(event.key === 'Enter'){
+      sendMessage();
+    } else if(event.key === 'Escape'){
+      setInputText('');
+    }
+  }
+
+  async function sendMessage() {
+    if(isLoading || inputText === ''){
+      return;
+    }
+    setIsLoading(true);
+
+    const newChatMessages = [
+      ...chatMessages, 
+      {
+        message: inputText,
+        sender: 'user',
+        id: crypto.randomUUID()
+      }
+    ]
+
+    setChatMessages([
+      ...newChatMessages, 
+      {
+        message: <img className="loading-spinner" src={LoadingGif} />,
+        sender: 'robot',
+        id: crypto.randomUUID()
+      }
+    ]);
+    setInputText('');
+
+    const response = await Chatbot.getResponseAsync(inputText);
+    setChatMessages([
+      ...newChatMessages, 
+      {
+        message: response,
+        sender: 'robot',
+        id: crypto.randomUUID()
+      }
+    ]);
+
+    setIsLoading(false);
+  }
+
+  return (
+    <div className="chat-input-container">
+      <input 
+        placeholder="Send a message to Chatbot" 
+        size="30" 
+        onChange={saveInputText}
+        onKeyDown={handleKeyDown}
+        value={inputText}
+        className="chat-input"
+      />
+      <button
+        onClick={sendMessage}
+        disabled={isLoading}
+        className="send-button"
+      >Send</button>
+    </div>
+  )
+}
---
+++ chatbot-project/src/components/ChatMessages.jsx
@@ -0,0 +1,22 @@
+import { useAutoScroll } from "./useAutoScroll";
+import { ChatMessage } from "./ChatMessage";
+
+function ChatMessages({ chatMessages }){
+  const chatMessagesRef = useAutoScroll([chatMessages])
+
+  return (
+    <div className="chat-messages-container" ref={chatMessagesRef}>
+      {chatMessages.map((chatMessage) => {
+        return (
+          <ChatMessage 
+            message={chatMessage.message}
+            sender={chatMessage.sender}
+            key={chatMessage.id}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default ChatMessages;
