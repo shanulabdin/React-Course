083194dce7cf7cc57480c6adf725607b7ba04209
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-12-14 00:04:11 +0500

    fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,6 +4,13 @@ import CheckmarkIcon from '../assets/images/icons/Checkmark.png';
 import './HomePage.css';
 
 export function HomePage() {
+  fetch('http://localhost:3000/api/products')
+    .then((response) => {
+      return response.json()
+    }).then((data) => {
+      console.log(data)
+    });
+
   return (
     <>
       <Header />
