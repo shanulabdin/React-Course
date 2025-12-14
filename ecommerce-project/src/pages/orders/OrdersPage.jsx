4f731c3ecd3018f4514b25518cd45403027fe99a
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-12-15 01:06:16 +0500

    Seperate orderDetailGrid into its component

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -1,11 +1,9 @@
 import axios from 'axios';
-import dayjs from 'dayjs';
-import { useState, useEffect, Fragment } from 'react'
+import { useState, useEffect } from 'react'
 import { Header } from '../../components/header';
-import { Link } from 'react-router';
-import BuyAgainIcon from '../../assets/images/icons/buy-again.png';
 import './OrdersPage.css';
 import { OrderHeader } from './OrderHeader';
+import { OrderDetailsGrid } from './OrderDetailsGrid';
 
 export function OrdersPage({ cart }) {
   const [orders, setOrders] = useState([]);
@@ -36,42 +34,8 @@ export function OrdersPage({ cart }) {
               <div key={order.id} className="order-container">
 
                 <OrderHeader order={order} />
-
-                <div className="order-details-grid">
-                  {order.products.map((orderProduct) => {
-                    return (
-                      <Fragment key={orderProduct.product.id}>
-                        <div className="product-image-container">
-                          <img src={orderProduct.product.image} />
-                        </div>
-
-                        <div className="product-details">
-                          <div className="product-name">
-                            {orderProduct.product.name}
-                          </div>
-                          <div className="product-delivery-date">
-                            Arriving on: {dayjs(orderProduct.estimatedDeliveryTimeMs).format('MMMM D')}
-                          </div>
-                          <div className="product-quantity">
-                            Quantity: {orderProduct.quantity}
-                          </div>
-                          <button className="buy-again-button button-primary">
-                            <img className="buy-again-icon" src={BuyAgainIcon} />
-                            <span className="buy-again-message">Add to Cart</span>
-                          </button>
-                        </div>
-
-                        <div className="product-actions">
-                          <Link href="/tracking">
-                            <button className="track-package-button button-secondary">
-                              Track package
-                            </button>
-                          </Link>
-                        </div>
-                      </Fragment>
-                    );
-                  })}
-                </div>
+                <OrderDetailsGrid order={order} />
+                
               </div>
             );
           })}
