fa04dc8892d267afec7d90b8b660fdce4416433e
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-12-15 01:02:12 +0500

    seperate orderHeader into its component

--- ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -5,7 +5,7 @@ import { Header } from '../../components/header';
 import { Link } from 'react-router';
 import BuyAgainIcon from '../../assets/images/icons/buy-again.png';
 import './OrdersPage.css';
-import { formatMoney } from '../../utils/money';
+import { OrderHeader } from './OrderHeader';
 
 export function OrdersPage({ cart }) {
   const [orders, setOrders] = useState([]);
@@ -35,23 +35,7 @@ export function OrdersPage({ cart }) {
             return (
               <div key={order.id} className="order-container">
 
-                <div className="order-header">
-                  <div className="order-header-left-section">
-                    <div className="order-date">
-                      <div className="order-header-label">Order Placed:</div>
-                      <div>{dayjs(order.orderTimeMs).format('MMMM d')}</div>
-                    </div>
-                    <div className="order-total">
-                      <div className="order-header-label">Total:</div>
-                      <div>{formatMoney(order.totalCostCents)}</div>
-                    </div>
-                  </div>
-
-                  <div className="order-header-right-section">
-                    <div className="order-header-label">Order ID:</div>
-                    <div>{order.id}</div>
-                  </div>
-                </div>
+                <OrderHeader order={order} />
 
                 <div className="order-details-grid">
                   {order.products.map((orderProduct) => {
