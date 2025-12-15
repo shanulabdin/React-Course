ab041d95c2366ceb4b77be6a014d1fcdceffee35
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-12-15 12:19:24 +0500

    now it actually works

--- ecommerce-project/src/pages/orders/OrderDetailsGrid.jsx
+++ ecommerce-project/src/pages/orders/OrderDetailsGrid.jsx
@@ -30,7 +30,7 @@ export function OrderDetailsGrid({ order }) {
             </div>
 
             <div className="product-actions">
-              <Link href={`/tracking/${order.id}/${orderProduct.product.id}`}>
+              <Link to={`/tracking/${order.id}/${orderProduct.product.id}`}>
                 <button className="track-package-button button-secondary">
                   Track package
                 </button>
