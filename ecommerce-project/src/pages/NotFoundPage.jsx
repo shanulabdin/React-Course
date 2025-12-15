605108d27a6417d2df7775c9a9ab70917b9e4e03
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-12-15 16:47:42 +0500

    added cart to header in notFoundPage

--- ecommerce-project/src/pages/NotFoundPage.jsx
+++ ecommerce-project/src/pages/NotFoundPage.jsx
@@ -1,13 +1,13 @@
 import { Header } from "../components/header";
 import './NotFoundPage.css';
 
-export function NotFoundPage(){
+export function NotFoundPage({ cart }){
   return (
     <div>
       <title>404 Page Not Found</title>
       <link rel="icon" href="home-favicon.png" />
 
-      <Header />
+      <Header cart={cart} />
       <p className="not-found-message">Page Not Found</p>
     </div>
   );
