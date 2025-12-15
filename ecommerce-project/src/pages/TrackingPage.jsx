0815be133131a86e7e1d81ad3d0d0087721e6645
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-12-15 11:53:05 +0500

    made the tracling page working

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -2,47 +2,49 @@ import './TrackingPage.css';
 import { Header } from '../components/header';
 import { Link } from 'react-router';
 
-export function TrackingPage(){
+
+export function TrackingPage({ cart }) {
   return (
     <>
-      <Header />
-
+      <title>Tracking</title>
       <link rel="icon" type="image/svg+xml" href="tracking-favicon.png" />
 
-      <div class="tracking-page">
-        <div class="order-tracking">
-          <Link class="back-to-orders-link link-primary" href="/orders">
+      <Header cart={cart} />
+
+      <div className="tracking-page">
+        <div className="order-tracking">
+          <Link className="back-to-orders-link link-primary" href="/orders">
             View all orders
           </Link>
 
-          <div class="delivery-date">
+          <div className="delivery-date">
             Arriving on Monday, June 13
           </div>
 
-          <div class="product-info">
+          <div className="product-info">
             Black and Gray Athletic Cotton Socks - 6 Pairs
           </div>
 
-          <div class="product-info">
+          <div className="product-info">
             Quantity: 1
           </div>
 
-          <img class="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+          <img className="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg" />
 
-          <div class="progress-labels-container">
-            <div class="progress-label">
+          <div className="progress-labels-container">
+            <div className="progress-label">
               Preparing
             </div>
-            <div class="progress-label current-status">
+            <div className="progress-label current-status">
               Shipped
             </div>
-            <div class="progress-label">
+            <div className="progress-label">
               Delivered
             </div>
           </div>
 
-          <div class="progress-bar-container">
-            <div class="progress-bar"></div>
+          <div className="progress-bar-container">
+            <div className="progress-bar"></div>
           </div>
         </div>
       </div>
