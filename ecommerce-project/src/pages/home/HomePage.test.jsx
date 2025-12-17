62889aad7bd1ff2e90c80ffae4c51da9489dd436
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-12-17 20:54:59 +0500

    updating quantity and texting again

--- ecommerce-project/src/pages/home/HomePage.test.jsx
+++ ecommerce-project/src/pages/home/HomePage.test.jsx
@@ -76,20 +76,28 @@ describe('HomePage component', () => {
 
     const addToCartButton1 = within(productContainers[0])
       .getByTestId('add-to-cart-button');
+    const quantitySelector1 = within(productContainers[0])
+      .getByTestId('quantity-selector');
+
+    await user.selectOptions(quantitySelector1, '2');
     await user.click(addToCartButton1);
     
     expect(axios.post).toHaveBeenNthCalledWith(1, '/api/cart-items', {
       productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-      quantity: 1
+      quantity: 2
     })
 
     const addToCartButton2 = within(productContainers[1])
       .getByTestId('add-to-cart-button');
+    const quantitySelector2 = within(productContainers[1])
+      .getByTestId('quantity-selector');
+      
+    await user.selectOptions(quantitySelector2, '3')
     await user.click(addToCartButton2);
 
     expect(axios.post).toHaveBeenNthCalledWith(2, '/api/cart-items', {
       productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-      quantity: 1
+      quantity: 3
     })
 
     expect(loadCart).toHaveBeenCalledTimes(2);
