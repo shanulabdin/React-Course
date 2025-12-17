014a3e0c00d8e5260aca7d146947fd9f876895c1
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-12-17 19:52:49 +0500

    setting user before each test

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -9,6 +9,7 @@ vi.mock('axios');
 describe('Product component', () => {
   let product;
   let loadCart;
+  let user;
 
   beforeEach(() => {
     product = {
@@ -23,6 +24,7 @@ describe('Product component', () => {
       keywords: ["socks", "sports", "apparel"]
     };
     loadCart = vi.fn();
+    user = userEvent.setup();
   })
 
   it('displays the product details correctly', () => {
@@ -52,7 +54,6 @@ describe('Product component', () => {
   it('adds a product to the cart', async () => {
     render(<Product product={product} loadCart={loadCart} />);
 
-    const user = userEvent.setup();
     const addToCartButton = screen.getByTestId('add-to-cart-button');
     await user.click(addToCartButton);
 
@@ -71,7 +72,6 @@ describe('Product component', () => {
     const quantitySelector = screen.getByTestId('quantity-selector');
     expect(quantitySelector).toHaveValue('1');
     
-    const user = userEvent.setup();
     await user.selectOptions(quantitySelector, '3');
     expect(quantitySelector).toHaveValue('3');
     
