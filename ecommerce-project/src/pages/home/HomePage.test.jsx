5f0549a62bd4f4ff5210a0c022ea78d36946b384
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-12-17 20:35:25 +0500

    test if the add to cart buttons work

--- ecommerce-project/src/pages/home/HomePage.test.jsx
+++ ecommerce-project/src/pages/home/HomePage.test.jsx
@@ -3,14 +3,17 @@ import { render, screen, within } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 import axios from 'axios';
 import { HomePage } from './HomePage';
+import userEvent from '@testing-library/user-event';
 
 vi.mock('axios');
 
 describe('HomePage component', () => {
   let loadCart;
+  let user;
 
   beforeEach(() => {
     loadCart = vi.fn();
+    user = userEvent.setup();
 
     axios.get.mockImplementation(async (urlPath) => {
       if (urlPath === '/api/products') {
@@ -54,11 +57,41 @@ describe('HomePage component', () => {
 
     expect(
       within(productContainers[0])
-      .getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')
+        .getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')
     ).toBeInTheDocument();
     expect(
       within(productContainers[1])
-      .getByText('Intermediate Size Basketball')
+        .getByText('Intermediate Size Basketball')
     ).toBeInTheDocument();
   });
+
+  it('adds the products to the cart', async () => {
+    render(
+      <MemoryRouter>
+        <HomePage cart={[]} loadCart={loadCart} />
+      </MemoryRouter>
+    );
+
+    const productContainers = await screen.findAllByTestId('product-container');
+
+    const addToCartButton1 = within(productContainers[0])
+      .getByTestId('add-to-cart-button');
+    await user.click(addToCartButton1);
+    
+    expect(axios.post).toHaveBeenNthCalledWith(1, '/api/cart-items', {
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+      quantity: 1
+    })
+
+    const addToCartButton2 = within(productContainers[1])
+      .getByTestId('add-to-cart-button');
+    await user.click(addToCartButton2);
+
+    expect(axios.post).toHaveBeenNthCalledWith(2, '/api/cart-items', {
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1
+    })
+
+    expect(loadCart).toHaveBeenCalledTimes(2);
+  })
 });
