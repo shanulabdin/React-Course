bb1a7521033d91c728dd64f518f1e8533e23c460
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-12-22 20:11:11 +0500

    favicon and title of prj, exercise 1a and 1b

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -3,6 +3,7 @@ import { ChatInput } from './components/ChatInput';
 import ChatMessages from './components/ChatMessages';
 import './App.css';
 import { Chatbot } from 'supersimpledev';
+import RobotPfp from './assets/robot.png'
 
 function App(){
   const [chatMessages, setChatMessages] = useState(JSON.parse(localStorage.getItem('messages')) || []);
@@ -32,8 +33,14 @@ function App(){
     localStorage.setItem('messages', JSON.stringify(chatMessages));
   })
 
+  const title = chatMessages.length + " Messages";
+
   return (
     <div className="app-container">
+      <title>{title}</title>
+
+      <link rel="icon" type="image/svg+xml" href={RobotPfp} />
+
       {chatMessages.length === 0 && (
         <p className="wellcome-message" >Welcome to the chatbot project! Send a message using the textbox below.</p>
       )}
